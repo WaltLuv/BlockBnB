31d65666a4a091ea373906d488ec47854be5b721
Walter Thornton <wjt17@hotmail.com>
2023-10-27 23:21:09 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const nextConfig = {
       'avatars.dicebear.com',
       'upcdn.io',
       't4.ftcdn.net',
+      'https://tomato-recent-halibut-222.mypinata.cloud/ipfs/QmV4afirJ3ETdZ4fi1z28gKdGHp4pTDj5ydwiy8GTmr9wm?pinataGatewayToken=rxbfJwqWt95BLT5tYJpRnWPBYwjhS-1IYpvfntaKXAr9dK2gDPDOj_m9bXcVnO5c&_gl=1*vma6ks*_ga*MTcwOTI2NDY3Mi4xNjk1OTQ1ODUz*_ga_5RMPXG14TE*MTY5ODQ2MzA5MC42LjAuMTY5ODQ2MzA5MC42MC4wLjA.',
+      'tomato-recent-halibut-222.mypinata.cloud'
     ],
   },
 }
