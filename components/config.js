1000f5b898a57d18388d75e754b86b66e03381fe
Walter Thornton <wjt17@hotmail.com>
2023-10-27 21:58:21 -0400

    Update config.js

--- components/config.js
+++ components/config.js
@@ -9,4 +9,11 @@ console.log(data);*/}
 
 
 
-export const nftcontract = "0x37Fe871E299ba7C41865F072DC4F614c6f19EEB9";
+//export const nftcontract = "0x37Fe871E299ba7C41865F072DC4F614c6f19EEB9";
+
+export const nftcontract = "0xdE03B12A987b94F0B649B576ae39F548edD7339e";
+
+
+
+
+//0xdE03B12A987b94F0B649B576ae39F548edD7339e
